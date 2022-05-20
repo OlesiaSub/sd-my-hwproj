f9d8d215205dd3b0dc7d48f203fc88de2da25922
Karina5005 <anisimova5005@gmail.com>
2022-05-20 18:24:48 +0300

    add submit hw

--- app/controllers/controller_student.py
+++ app/controllers/controller_student.py
@@ -12,17 +12,27 @@ class ControllerStudent:
     Base.metadata.create_all(bind=engine)
 
     def get_hw_sorted(self):
-        db = self.databaseServer.get_db()
-        hws = next(db).query(schemas.Homework).filter(schemas.Homework.publication_date <= now()) \
+        db = next(self.databaseServer.get_db())
+        hws = db.query(schemas.Homework).filter(schemas.Homework.publication_date <= now()) \
             .order_by(schemas.Homework.deadline).all()
         return list(
             map(lambda x: models.Homework(full_name=x.full_name, task_text=x.task_text, deadline=x.deadline,
                                           publication_date=x.publication_date), hws))
 
     def get_results_sorted(self):
-        db = self.databaseServer.get_db()
-        results = next(db).query(schemas.Result).order_by(schemas.Result.data).all()
+        db = next(self.databaseServer.get_db())
+        results = db.query(schemas.Result).order_by(schemas.Result.data).all()
         return list(map(lambda x: models.Result(comment=x.comment, mark=x.mark, data=x.data), results))
 
     def submit_hw(self, hw_id, attempt: Attempt):
-        pass
+        db = next(self.databaseServer.get_db())
+        db_attempt = schemas.Attempt(homework_id=hw_id,
+                                     solution=attempt.solution)
+        db.add(db_attempt)
+        db.commit()
+        db.refresh(db_attempt)
+        result = schemas.Result(data=now(), attempt_id=db_attempt.id, mark=5, comment="good")
+        db.add(result)
+        db.commit()
+        db.refresh(result)
+        return models.Result(comment=result.comment, mark=result.mark, data=result.data)
