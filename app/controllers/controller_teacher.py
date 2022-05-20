73fe5f9e3edefbc1452d0f0770f9b1e1d5739294
OlesiaSub <lesya.sub@mail.ru>
2022-05-20 20:33:13 +0300

    Let teacher add hw

--- app/controllers/controller_teacher.py
+++ app/controllers/controller_teacher.py
@@ -15,10 +15,8 @@ class ControllerTeacher:
         results = next(db).query(schemas.Result).order_by(schemas.Result.data).all()
         return list(map(lambda x: models.Result(comment=x.comment, mark=x.mark, data=x.data), results))
 
-    def add_hw(self, homework: models.Homework, db: Session):
-        return self.create_hw(homework=homework, db=db)
-
-    def create_hw(self, homework: models.Homework, db: Session):
+    def add_hw(self, homework: models.Homework):
+        db = next(self.databaseServer.get_db())
         db_homework = schemas.Homework(full_name=homework.full_name,
                                        deadline=homework.deadline,
                                        publication_date=homework.publication_date,
@@ -26,7 +24,7 @@ class ControllerTeacher:
         db.add(db_homework)
         db.commit()
         db.refresh(db_homework)
-        return db_homework
 
+    # todo
     def update_checker(self):
         pass
--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 from fastapi import FastAPI, Depends, Form
 from fastapi_utils.cbv import cbv
@@ -5,11 +6,13 @@ from fastapi_utils.inferring_router import InferringRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import now
 from starlette.requests import Request
 
 from app.controllers.controller_student import ControllerStudent
 from app.controllers.controller_teacher import ControllerTeacher
 from app.form.attempt import AttemptCreateForm
+from app.form.new_hw import NewHwCreateForm
 from app.models import models
 from app.logic.database_server import DatabaseServer
 
@@ -39,7 +42,7 @@ class Server:
                                           {"request": request, "results": self.controllerStudent.get_results_sorted()})
 
     @router.get("/student/hw/{id}/submit")
-    def create_attempt(self, request: Request, id:int):
+    def create_attempt(self, request: Request, id: int):
         return templates.TemplateResponse("create_attempt.html", {"request": request})
 
     @router.post("/student/hw/{id}/submit", response_model=models.Result)
@@ -51,14 +54,32 @@ class Server:
             self.controllerStudent.submit_hw(id, attempt)
         return templates.TemplateResponse("create_attempt.html", form.__dict__)
 
-    @router.post("/teacher/new_homework")
-    def add_homework(self, homework: models.Homework, db: Session = Depends(databaseServer.get_db)):
-        return self.controllerTeacher.create_hw(homework, db)
+    @router.get("/teacher/results", response_model=List[models.Result])
+    def get_teacher_results(self, request: Request):
+        return templates.TemplateResponse("result_list.html",
+                                          {"request": request, "results": self.controllerTeacher.get_results_sorted()})
 
     @router.get("/teacher/results", response_model=List[models.Result])
     def get_teacher_results(self, request: Request):
         return templates.TemplateResponse("result_list.html",
                                           {"request": request, "results": self.controllerTeacher.get_results_sorted()})
+
+    @router.get("/teacher/new_homework")
+    def form_post(self, request: Request):
+        return templates.TemplateResponse('new_hw.html', context={'request': request})
+
+    @router.post("/teacher/new_homework")
+    async def hw_post(self, request: Request):
+        form = NewHwCreateForm(request)
+        await form.load_data()
+        deadline = form.deadline.replace('-', '/')
+        date_time_deadline = datetime.strptime(deadline[2:], '%y/%m/%d')
+        date = datetime.now()
+        date = date.replace(day=date.day - 1)
+        hw = models.Homework(full_name=form.name, task_text=form.text, deadline=date_time_deadline,
+                             publication_date=date)
+        self.controllerTeacher.add_hw(homework=hw)
+        return templates.TemplateResponse("new_hw.html", form.__dict__)
 
 
 server = Server()
